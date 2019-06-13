c09edb666ecd36db20409b26443bf6b8ec4b4b22
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-06-13 19:31:43 +0100

    Create 3 VCs and UIViewController extension

---
+++ GifMaker_Swift_Template/UIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Tanin on 13/06/2019.
+//  Copyright © 2019 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+
+extension UIViewController {
+  
+  @IBAction func presentVideoOptions(sender: AnyObject) {
+    
+    // create imagePicker
+    // set properties: sourcetype, mediatype, allowsEditing, delegate
+    // present controller
+    
+  }
+  
+}
