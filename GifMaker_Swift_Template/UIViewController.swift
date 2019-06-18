13f46dec1e51023a6a0315c75fce20e82fb64d7e
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-06-18 19:22:44 +0100

    Add UIImage and UIViewController extensions

--- GifMaker_Swift_Template/UIViewController.swift
+++ GifMaker_Swift_Template/UIViewController.swift
@@ -10,14 +10,42 @@ import Foundation
 import UIKit
 import MobileCoreServices
 
-extension UIViewController {
+extension UIViewController: UINavigationControllerDelegate {
   
   @IBAction func presentVideoOptions(sender: AnyObject) {
     
     // create imagePicker
     // set properties: sourcetype, mediatype, allowsEditing, delegate
     // present controller
+    let recordVideoController = UIImagePickerController()
+    recordVideoController.sourceType = .camera
+    recordVideoController.mediaTypes = [kUTTypeMovie as String]
+    recordVideoController.allowsEditing = false
+    recordVideoController.delegate = self
     
+    present(recordVideoController, animated: true, completion: nil)
+    
+  }
+  
+}
+
+extension UIViewController: UIImagePickerControllerDelegate {
+  
+  public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    
+    let mediaType = info[UIImagePickerControllerMediaType] as! String
+    
+    if mediaType == kUTTypeMovie as String {
+      let videoURL = info[UIImagePickerControllerMediaURL] as! NSURL
+      UISaveVideoAtPathToSavedPhotosAlbum(videoURL.path!, nil, nil, nil)
+      dismiss(animated: true, completion: nil)
+    }
+    
+    
+  }
+  
+  public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    dismiss(animated: true, completion: nil)
   }
   
 }
