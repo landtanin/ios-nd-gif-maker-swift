cd554b65f7e29427f4a7ce7d9bdae229bb0f9335
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-07-05 18:40:50 +0100

    Implementing textFieldDelegate in GifEditorVC

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -14,11 +14,60 @@ class GifEditorViewController: UIViewController {
   @IBOutlet weak var captionTextField: UITextField!
   var gif:Gif?
   
+  override func viewDidLoad() {
+    captionTextField.delegate = self
+  }
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    subscribeToKeyboardNotifications()
     if let gif = gif {
       gifImageView.image = gif.gifImage
     }
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    unsubscribeFromKeyboardNotifications()
+  }
+  
+}
+
+extension GifEditorViewController: UITextFieldDelegate {
+  
+  func subscribeToKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: .UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: .UIKeyboardWillHide, object: nil)
+  }
+  
+  func unsubscribeFromKeyboardNotifications() {
+    NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+  }
+  
+  @objc func keyboardWillShow(sender: AnyObject) {
+    if self.view.frame.origin.y >= 0 {
+      var rect = self.view.frame
+//      rect.origin.y -=
+    }
+  }
+  
+  @objc func keyboardWillHide(sender: AnyObject) {
+    
+  }
+  
+  func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+//    let userInfo = notification.userInfo
+    
+  }
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    textField.placeholder = "";
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true;
   }
   
 }
